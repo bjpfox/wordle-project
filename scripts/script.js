4b394b48e7bf18bd8ebf8d5f502b003328a0c4b3
Brendan <brendan@Brendans-MacBook-Pro.local>
2023-01-18 20:22:02 +1100

    Added event listeners to keys in index.html

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,7 @@ const buttons = document.querySelectorAll('button')
 for (let button of buttons) {
     button.addEventListener('click', function(event) {
         console.log(`You clicked: ${event.target.innerText}`)
+        event.target.classList.remove('keyboard-letter-not-selected')
+        event.target.classList.add('keyboard-letter-correct')
     })
 }
